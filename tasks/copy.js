3a054a373bc3964f2426937b5871cc94b2cfe263
edsilv <e.silverton@gmail.com>
2016-06-05 14:02:46 +0100

    use path

--- tasks/copy.js
+++ tasks/copy.js
@@ -2,9 +2,10 @@ var c = require('../gulpfile.config');
 var config = new c();
 var gulp = require('gulp');
 var utils = require('gulp-utils');
+var path = require('path');
 
 gulp.task('copy:bundle', function() {
-    return gulp.src(config.dist + '/' + config.jsBundleOut).pipe(gulp.dest(config.testDepsDir));
+    return gulp.src(path.join(config.dist, config.jsBundleOut)).pipe(gulp.dest(config.testDepsDir));
 });
 
 gulp.task('copy:typings', function() {
